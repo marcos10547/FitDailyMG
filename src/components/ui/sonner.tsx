9a194da5e105df2762738e945bfa1aa7940b0421
marcos10547 <160898439+marcos10547@users.noreply.github.com>
2026-03-14 21:32:03 +0100

    cambios

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,8 +1,7 @@
-import { useTheme } from 'next-themes'
 import { Toaster as Sonner, ToasterProps } from 'sonner'
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = 'system' } = useTheme()
+  const theme = 'light'
 
   return (
     <Sonner
